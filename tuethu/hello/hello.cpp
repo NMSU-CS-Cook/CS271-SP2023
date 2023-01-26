ec75113c3cce859a0facc0261308ab74dda56c36
Jonathan Cook <joncook@nmsu.edu>
2023-01-26 16:31:19 -0700

    in class thu 26 jan

--- tuethu/hello/hello.cpp
+++ tuethu/hello/hello.cpp
@@ -3,4 +3,7 @@
 int main(int argc, char *argv[])
 {
     std::cout << "Hello World!\n";
+    for (int i=0; i<argc; i++) {
+        std::cout << "arg: ("+std::string(argv[i])+")\n";
+    }
 }
--- tuethu/java/Hello.java
+++ tuethu/java/Hello.java
@@ -5,6 +5,9 @@ public class Hello
 public static void main(String args[])
 {
     System.out.println("Hello World!");
+    for (String arg: args) {
+        System.out.println("arg: ("+arg+")");
+    }
 }
 
 } // end class
