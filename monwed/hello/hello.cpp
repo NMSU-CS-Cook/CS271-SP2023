07b7dd034a0ad2d6f39dbe3a758161aaf9956644
Jonathan Cook <joncook@nmsu.edu>
2023-01-26 12:20:54 -0700

    wed 25jan class

--- monwed/hello/hello.cpp
+++ monwed/hello/hello.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World\n";
-    std::cout << "Goodbye!\n";
+    std::cout << "Hello World!\n";
+    for (int i=1; i < argc; i++) {
+        std::string s = argv[i];
+        std::cout << "arg ("+s+")\n";
+    }
 }
--- monwed/java/Hello.java
+++ monwed/java/Hello.java
@@ -8,6 +8,10 @@ public class Hello
 public static void main(String args[])
 {
     System.out.println("Hello World!");
+    System.out.println("Length of args: "+args.length);
+    for (String s: args) {
+       System.out.println("arg: ("+s+")");
+    }
 }
 
 } // end class
