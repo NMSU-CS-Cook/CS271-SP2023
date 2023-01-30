2d16b8f15a3b7724151207e810bd23fbcfb0403f
Jonathan Cook <joncook@nmsu.edu>
2023-01-30 15:39:59 -0700

    mon jan 30 in class

--- monwed/hello/hello.cpp
+++ monwed/hello/hello.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <string>
 
+void printArray(int array[])
+{
+    int i = 0;
+    while (i < 10)
+    {
+        std::cout << "vals[" << i << "] = "
+             << array[i] << "\n";
+        i++;
+    }
+    array[3] = 1234;
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello World!\n";
-    for (int i=1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         std::string s = argv[i];
-        std::cout << "arg ("+s+")\n";
+        std::cout << "arg (" + s + ")\n";
     }
+    // In class Jan 30
+    int vals[10];
+    for (int i = 0; i < 10; i++)
+    {
+        vals[i] = i + 20;
+    }
+    printArray(vals);
+    std::cout << "vals[3]=" << vals[3] << "\n";
+    return 0;
 }
--- monwed/java/Hello.java
+++ monwed/java/Hello.java
@@ -5,13 +5,29 @@
 public class Hello
 {
 
+static void printArray(int array[])
+{
+    for (int i=0; i < 10; i++) {
+        System.out.println("vals["+i+"] = "+array[i]);
+    }
+    array[3] = 1234;
+}
+
 public static void main(String args[])
 {
+    int x;
     System.out.println("Hello World!");
     System.out.println("Length of args: "+args.length);
     for (String s: args) {
        System.out.println("arg: ("+s+")");
     }
+    int vals[];
+    vals = new int[10];
+    for (int i=0; i < 10; i++) {
+        vals[i] = i+20;
+    }
+    printArray(vals);
+    System.out.println("vals[3]="+vals[3]);
 }
 
 } // end class
