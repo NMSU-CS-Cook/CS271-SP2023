4e0ea5b11adbf199660536ca8c89ee14018328f5
Jonathan Cook <joncook@nmsu.edu>
2023-02-02 13:03:34 -0700

    tue 31 jan

--- tuethu/hello/hello.cpp
+++ tuethu/hello/hello.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
+void testOperator()
+{
+   int v = 4;
+   std::cout << "v is " << v << "\n";
+   v = v << 2;
+   std::cout << "v is " << v << "\n";
+}
+
+void printArray(int array[])
+{
+    int i = 0;
+    while (i < 10) {
+        std::cout << "array[" << i << "] = " << array[i] << "\n";
+        i++;
+    }
+    array[3] = 1234;
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello World!\n";
     for (int i=0; i<argc; i++) {
         std::cout << "arg: ("+std::string(argv[i])+")\n";
     }
+    int vals[10];
+    for (int i=0; i < 10; i++) {
+        vals[i] = i+20;
+    }
+    //std::cout << "array size is " << vals.length << "\n";
+    printArray(vals);
+    std::cout << "vals[3]=" << vals[3] << "\n";
+    testOperator();
+    return 0;
 }
--- tuethu/java/Hello.java
+++ tuethu/java/Hello.java
@@ -2,12 +2,30 @@
 public class Hello
 {
 
+static void printArray(int array[])
+{
+    System.out.println("Array size is " + array.length);
+    int i = 0;
+    while (i < 10) {
+        System.out.println("array["+i+"] = "+array[i]);
+        i++;
+    }
+    array[3] = 1234;
+}
+
 public static void main(String args[])
 {
     System.out.println("Hello World!");
     for (String arg: args) {
         System.out.println("arg: ("+arg+")");
     }
+    int[] vals;
+    vals = new int[10];
+    for (int i=0; i < 10; i++) {
+        vals[i] = i+20;
+    }
+    printArray(vals);
+    System.out.println("vals[3] = "+vals[3]);
 }
 
 } // end class
